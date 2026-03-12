d61d6b1961beda393e1d6237b3e4b1d1d46de43d
Diogo Moreira <diogomoreira1910@gmail.com>
2026-03-12 11:39:37 -0300

    feat jwt security

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,17 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
         PassportModule,
         JwtModule.register({
-            secret: process.env.JWT_SECRET,
+            secret: process.env.JWT_SECRET as string,
             signOptions: { expiresIn: '1d' },
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule { }
